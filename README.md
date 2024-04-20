1fb2d6153f4c9c485fdf1d6ca9c9adad84ce8df3
Alexey Kuznetsov <a.kuznetsov@okft.io>
2024-04-21 02:20:46 +0500

    Add pluck names batch

--- README.md
+++ README.md
@@ -2,3 +2,35 @@
 
 1. docker build -t natasha-api .
 2. docker run -it --rm -p 3000:80 natasha-api
+
+
+# Usage
+
+## pluck_names
+Request:
+```
+POST /pluck_names
+
+Body (JSON):
+{"text": "Some text here..."}
+```
+Response JSON:
+```
+["Name 1", "Name 2"]
+```
+
+## pluck_names_batch
+Request:
+```
+POST /pluck_names_batch
+
+Body (JSON):
+{"text_batch": ['"Some text here...", ...]}
+```
+Response JSON:
+```
+[
+  ["Name 1", "Name 2"],
+  ...
+]
+```
--- main.py
+++ main.py
@@ -2,33 +2,50 @@ from fastapi import FastAPI
 from natasha import (
     Segmenter,
     MorphVocab,
-    
+
     NewsEmbedding,
     NewsMorphTagger,
     NewsSyntaxParser,
     NewsNERTagger,
-    
+
     PER,
     NamesExtractor,
 
     Doc
 )
 from pydantic import BaseModel
+from typing import List
 
-class Item(BaseModel):
+class PluckNamesInput(BaseModel):
     text: str
+
+class PluckNamesBatchInput(BaseModel):
+    text_batch: List[str]
 
 app = FastAPI()
 
 @app.post("/pluck_names")
-async def pluck_names(item: Item):
+async def pluck_names(input: PluckNamesInput):
   emb = NewsEmbedding()
   segmenter = Segmenter()
   ner_tagger = NewsNERTagger(emb)
-  doc = Doc(item.text)
+  doc = Doc(input.text)
   doc.segment(segmenter)
   doc.tag_ner(ner_tagger)
   return extract_names(doc.spans)
+
+@app.post("/pluck_names_batch")
+async def pluck_names_batch(input: PluckNamesBatchInput):
+  emb = NewsEmbedding()
+  segmenter = Segmenter()
+  ner_tagger = NewsNERTagger(emb)
+  batch = []
+  for text in input.text_batch:
+    doc = Doc(text)
+    doc.segment(segmenter)
+    doc.tag_ner(ner_tagger)
+    batch.append(extract_names(doc.spans))
+  return batch
 
 def extract_names(spans):
     names = []
